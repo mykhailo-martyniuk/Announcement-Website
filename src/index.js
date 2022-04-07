8b06c71cd5b70cab00ba5dcf638f5894e54d8a8c
Mykhasyk <mishaukrain999@gmail.com>
2022-04-07 17:45:03 +0300

    init

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
